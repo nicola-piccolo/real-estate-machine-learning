d5bd40c557225d350cbf0e61f21c1944c0c5e788
nicola <nickhannibal@gmail.com>
2021-03-13 19:10:12 +0900

    Created separated shell script for production with support for scaling

--- real-estate-ml-compose.sh
+++ real-estate-ml-compose.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-if [ "${1,,}" != '--development' ] && [ "${1,,}" != '--production' ] || [ "${2,,}" != 'up' ] && [ "${2,,}" != 'down' ] 
+servers_number=1
+if [ "${1,,}" == '--scale' ]
 then
-    echo "Usage:  real-estate-ml-compose options COMMAND
+    shift
+    servers_number="$1"
+    shift
+fi
+if [ "${1,,}" != 'up' ] && [ "${1,,}" != 'down' ] 
+then
+    echo "Usage:  real-estate-ml-compose [options] COMMAND
     
     options:
-        --development   runs the application in development mode
-        --production    runs the application in production mode
+        --scale sets the number of servers for the real estate ML application
 
     COMMAND:
         up      bootstrap the whole cluster
         down    tear down the cluster"
     exit 1
 fi
-if [ "${1,,}" == '--production' ]
+if [ "${1,,}" == 'up' ]
 then
-    export APPLICATION_ENVIRONMENT=production
-else
-    export APPLICATION_ENVIRONMENT=development
-fi
-if [ "${2,,}" == 'up' ]
-then
-    exec docker-compose --file ./docker/docker-compose.yml up --detach
+    exec docker-compose --file ./docker/docker-compose.yml up --detach --scale real-estate-ml=$servers_number
     exit 0
 fi
-if [ "${2,,}" == 'down' ]
+if [ "${1,,}" == 'down' ]
 then
     exec docker-compose --file ./docker/docker-compose.yml down
     exit 0
